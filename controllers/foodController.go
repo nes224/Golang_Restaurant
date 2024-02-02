47a6bc57c6f94d5a5bff038a08b5db1700f654b4
nes224 <nes224@hotmail.com>
2024-02-02 14:45:22 +0700

    CreateFood Controller

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,6 +1,18 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"golang-restaurant-management/database"
+	"golang-restaurant-management/models"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/mongo"
+	"gopkg.in/mgo.v2/bson"
+)
+
+var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 func GetFoods() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -16,7 +28,17 @@ func GetFood() gin.HandlerFunc {
 
 func CreateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		foodId := c.Param("food_id")
+		var food models.Food
 
+		err := foodCollection.FindOne(ctx, bson.M{"food_id":foodId}).Decode(&food)
+		defer cancel()
+		if err == nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error":"error occured while fetching item food"})
+		}
+
+		c.JSON(http.StatusOK,food)
 	}
 }
 
@@ -25,7 +47,7 @@ func round(num float64) int {
 }
 
 func toFixed(num float64, precision int) float64 {
-	
+
 }
 
 func UpdateFood() gin.HandlerFunc {
